6b12ee5c32bb2fd67f904f22264429f335063620
jona <jevkjev@gmail.com>
2022-11-14 04:40:09 -0300

    mongodb4

--- database.js
+++ database.js
@@ -1,8 +1,9 @@
-const mongoose = require('mongoose');
-const URI = 'mongodb://localhost/tp-final';
+const mongoose = require("mongoose");
+const URI = "mongodb://mongodb4/tp-final";
 
-mongoose.connect(URI)
-    .then(db => console.log("BD conectada"))
-    .catch(err => console.error(err))
+mongoose
+  .connect(URI)
+  .then((db) => console.log("BD conectada"))
+  .catch((err) => console.error(err));
 
-module.exports= mongoose;
+module.exports = mongoose;
